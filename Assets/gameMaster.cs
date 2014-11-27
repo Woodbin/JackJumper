aaa11c488a32ffa910c8ed4a4a85ec9944b8ca70
jjunior776 <juniorn@seznam.cz>
2014-11-27 13:44:02 +0100

    Prirazena animace skoku, pridanej pickUp + skore do UI

---
+++ Assets/gameMaster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameMaster : MonoBehaviour {
+	public static int currentScore = 0;
+	int sizeX= 100;
+	int sizeY= 25;
+	int positionY=0;
+	void OnGUI () {
+		//Debug.Log (currentScore);  -->test
+		GUI.Box (new Rect((Screen.width/2)-sizeX/2,positionY,sizeX,sizeY), "Score: "+currentScore);
+
+	}
+
+}
---
+++ Assets/pickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUp : MonoBehaviour {
+	int valueOfCoin = 5;
+	void OnTriggerEnter2D(Collider2D something){
+		if (something.tag == "Player") {
+
+						//Debug.Log ("spirit sebran");			//-->TEST!
+						gameMaster.currentScore += 1*valueOfCoin;
+						Destroy (gameObject);
+				}
+	}
+
+}
